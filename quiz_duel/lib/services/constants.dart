78dc30a8f7b872a0935bfabc321cec5b0038e78b
@s_aura.v02.15 <nyros.astral6114@gmail.com>
2026-07-27 15:39:24 +0545

    updated baseUrl on constants.dart

--- quiz_duel/lib/services/constants.dart
+++ quiz_duel/lib/services/constants.dart
@@ -1,6 +1,5 @@
 class AppConstants {
-  static const String baseUrl =
-      'https://quiz-royale-with-bert-working.onrender.com';
+  static const String baseUrl = 'https://quiz-royale-7g63.onrender.com';
   static const String apiUrl = '$baseUrl/api';
   static const String socketUrl = baseUrl;
 }
